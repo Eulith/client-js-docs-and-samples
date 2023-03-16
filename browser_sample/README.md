e12a9099ab1ac33a70f309421565c5f7e044ec8e
Lewis G. Pringle, Jr <lewis@sophists.com>
2023-03-16 18:38:47 -0400

    use CommonJS and improvements to browser sample

---
+++ browser_sample/README.md
@@ -0,0 +1,45 @@
+# Basic sample of Eulith client for web browser
+
+To make much use of this sample, you will need a server that is confiured to allow
+your client to connect via CORS.
+
+And you will need a URL for that server, and a refreshToken to use.
+
+NOTE - the html UI will prompt for these things - no need to build them into the code/configuration.
+
+### Scripts
+
+#### `npm run start`
+
+Starts the app in production by first building the project with `npm run build`, and then executing the compiled JavaScript at `build/index.js`.
+
+#### `npm run build`
+
+build-dev to build dev version, or build-prod to build for production
+
+#### `npm run format`
+
+Format your code.
+
+## Running build and seeing security erorr loading script
+
+Depending on your browser settings, you may not be able run html / JS directly from the filesystem
+
+In that case, this maybe helpful
+
+~~~
+cd dist
+npx http-server .
+~~~
+
+Then use the published link from http-server output to browse this web-app.
+
+
+## If your RPC server is generating CORS erorrs (doesn't include your particular GUI)
+
+You can run your client through a cors-enabling revsere proxy
+
+[https://www.npmjs.com/package/local-cors-proxy](https://www.npmjs.com/package/local-cors-proxy)
+~~~
+    npx lcp --proxyUrl http://localhost:7777 ??? SOMETHING LIKE THIS MUST PLAY/DONT RECALL
+~~~
--- browser_sample/src/index.ts
+++ browser_sample/src/index.ts
@@ -1,12 +1,17 @@
 import Web3 from "web3";
-// import * as Eulith from 'eulith-web3js-core';
-import * as Eulith from "eulith-web3js";
+import * as Eulith from "eulith-web3js";        // could use 'eulith-web3js-core' to reduce dependencies
+
+async function usingWindowFetch_(a1: any, a2: any) {
+    return await window.fetch(a1, a2);
+}
 
 async function doFetchChainID() {
     const providerArgs = {
         serverURL: (document.getElementById("serverURL") as any).value,
         refreshToken: (document.getElementById("refreshToken") as any).value,
+        // fetcherInstance: usingWindowFetch_           // defaults to axios, but do this to use fetch
     };
+
     const provider = new Eulith.Provider(providerArgs);
     // console.log("constructoed provider");
     const web3 = new Web3(provider);
@@ -20,23 +25,3 @@ async function doFetchChainID() {
     document.getElementById("chainID").innerText = chainID;
 }
 (window as any).doFetchChainID = doFetchChainID;
-
-// async function helloWorld(): Promise<string> {
-//     const provider = new Eulith.Provider(providerArgs);
-//     console.log("constructoed provider");
-//     const web3 = new Web3(provider);
-//     let chainID: string | undefined;
-//     try {
-//         chainID = await (await web3.eth.getChainId()).toString();
-//     } catch (e) {
-//         chainID = `Err: ${e.message}`;
-//     }
-//     return ["Hello", "webpack", "using", "chainid", chainID].join(" ");
-// }
-// async function component() {
-//     const element = document.createElement("div");
-//     element.innerHTML = await helloWorld();
-//     return element;
-// }
-
-// component().then((a) => document.body.appendChild(a));
